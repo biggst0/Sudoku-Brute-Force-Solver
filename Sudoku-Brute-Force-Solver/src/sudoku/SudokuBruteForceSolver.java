f372c8cb7293e705fa0e6cc8e50e7a6b071cdff7
biggst0 <biggst0@students.rowan.edu>
2015-09-22 22:00:55 -0400

    Update SudokuBruteForceSolver.java

--- Sudoku-Brute-Force-Solver/src/sudoku/SudokuBruteForceSolver.java
+++ Sudoku-Brute-Force-Solver/src/sudoku/SudokuBruteForceSolver.java
@@ -1,5 +1,15 @@
 package sudoku;
 
+/**
+ * This class provides the computation of solving a sudoku puzzle.
+ * The solution is printed along with a solved statement and the time taken in milliseconds.
+ * If the sudoku puzzle can not be solved the unsolved puzzle is printed along with a failed statement. 
+ *
+ * @author Travis Biggs
+ * @author Larry Romano
+ * @author Debra Lufadeju.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -17,64 +27,19 @@ public class SudokuBruteForceSolver
 	private int numCells;
 
 	/**
-	 * This method takes a text file and scans it. It takes the width and length
-	 * of the sudoku and creates a 2 dimension array. Then it loads the integers
-	 * into the array.
-	 * 
-	 * @param String
-	 *            of the location of the text file
-	 * @return
-	 * @return
-	 * @throws IOException
-	 *             If the text file cannot be found
+	 * The solverControl method takes a String parameter of the fileName of a Sudoku puzzle.
+	 * It then proceeds to initiate the sequence and check each test in a loop until the the puzzle is solved or determined unsolvable. 
+	 * @param sudokuPuzzle, Name of the Sudoku puzzle being solved.
+	 * @throws FileNotFoundException
 	 */
-	public void readFile(String fileName) throws FileNotFoundException {
-
-		Scanner sc = new Scanner(new File(fileName)); // starts the scanner
-
-		if ((!sc.hasNextInt()))// if the next input is not an int
-		{
-			sc.next().charAt(0); // remove the first character at the first
-									// position
-		}						
-		width = sc.nextInt();// Assigns next int to width
-		height = sc.nextInt();// Assigns next int to height
-	
-		magicNum = width * height;// assign width * height to the magicNum
-		testArray = new int[magicNum][magicNum];// Declare a 2Dimension array
-		refArray = new int[magicNum][magicNum];// Declare a 2Dimension array
-
-		int j = 0;// declare and initialize int j to 0
-		int i = 0;// declare and initialize int i to 0
-
-		int blankCounter = 0;
-		while (sc.hasNextInt()) {// while scanner still has a next int
-			int k = sc.nextInt();// assigned next int to variable k
-
-			testArray[i][j] = k;// insert variable k in the test array
-			refArray[i][j] = k;// insert variable k in the ref array
-			if (k == 0)// if variable k == 0
-				blankCounter++;// increase counter by 1
-			if (j == magicNum - 1)// if magicNum - 1 is equal to j
-			{
-				j = 0;// set j equal to j equal to zero
-				i++;// increase i by 1
-			} else// else
-			{
-				j++;// increase j by 1.
-			}
-
-		}
-		sequenceLength = blankCounter;
-		sc.close();// close scanner.
-
-	}
-
-	public void solverControl() throws FileNotFoundException 
+	public void solverControl(String sudokuPuzzle)
 	{
-		
-		readFile("C:/Users/Travis/git/Sudoku-Brute-Force-Solver/Sudoku-Brute-Force-Solver/src/soduku.txt");
-		
+		try {
+			readFile(sudokuPuzzle);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		initSequence();
 		boolean solved = false;
 		int i = 0;
@@ -91,15 +56,19 @@ public class SudokuBruteForceSolver
 		printInformation(solved);
 	}
 
-	// initializes sequence[] and sets all values to 1.
+	/**
+	 * The initSequence method initializes sequence[] and sets all values to 1.
+	 */
 	public void initSequence() {
 		sequence = new int[sequenceLength];
 		for (int i = 0; i < sequenceLength; i++)
 			sequence[i] = 1;
 	}
 
-	// increments sequence[] from left to right..
-	// returns true as long as there is another possible sequence value.
+	/**
+	 * The incrementSequence method increments the current sequence left to right by 1.
+	 * @return boolean, true as long as there is another possible sequence value and false otherwise.
+	 */
 	public boolean incrementSequence(){
 		int carry = 1;
 		int position = 0;
@@ -119,9 +88,56 @@ public class SudokuBruteForceSolver
 		}
 		return true;
 	}
+	
+	
+	/**
+	 * This method takes a text file and scans it. It takes the width and length
+	 * of the sudoku and creates a 2 dimension array. Then it loads the integers
+	 * into the array.
+	 * @param String of the location of the text file
+	 * @throws IOException If the text file cannot be found
+	 */
+	public void readFile(String fileName) throws FileNotFoundException {
 
-	// loads sequence[] values into testArray[][] using refArray[][] as a
-	// reference.
+		Scanner sc = new Scanner(new File(fileName));
+
+		if ((!sc.hasNextInt()))
+		{
+			sc.nextLine();
+		}	
+		width = sc.nextInt();
+		height = sc.nextInt();
+	
+		magicNum = width * height;
+		testArray = new int[magicNum][magicNum];
+		refArray = new int[magicNum][magicNum];
+
+		int j = 0;
+		int i = 0;
+
+		int blankCounter = 0;
+		while (sc.hasNextInt()) {
+			int k = sc.nextInt();
+
+			testArray[i][j] = k;
+			refArray[i][j] = k;
+			if (k == 0)
+				blankCounter++;
+			if (j == magicNum - 1){
+				j = 0;
+				i++;
+			}
+			else{
+				j++;
+			}
+		}
+		sequenceLength = blankCounter;
+		sc.close();
+	}
+
+	/**
+	 * The loadSequence method loads the current sequence into the testArray while using refArray as a template.
+	 */
 	public void loadSequence() {
 		int sequencePos = 0;
 		for (int rowCount = 0; rowCount < magicNum; rowCount++) {
@@ -134,8 +150,10 @@ public class SudokuBruteForceSolver
 		}
 	}
 
-	// checks each cell for a matching value in that cell's row.
-	// returns true if a match is found and false if a match is not.
+	/**
+	 * The checkCols method checks each row of the Sudoku puzzle. 
+	 * @return boolean, Returns false if the test fails and true if the test passes.
+	 */
 	public boolean checkRows() {
 		int currentRow = 0;
 		int currentCol = 0;
@@ -159,8 +177,10 @@ public class SudokuBruteForceSolver
 		return true;
 	}
 
-	// checks each cell for a matching value in that cell's column.
-	// returns true if a match is found and false if no match is found.
+	/**
+	 * The checkCols method checks each column of the Sudoku puzzle. 
+	 * @return boolean, Returns false if the test fails and true if the test passes.
+	 */
 	public boolean checkCols() {
 		int currentRow = 0;
 		int currentCol = 0;
@@ -184,6 +204,10 @@ public class SudokuBruteForceSolver
 		return true;
 	}
 
+	/**
+	 * The checkBoxes method is a test that checks the individual boxes of the Sudoku.
+	 * @return boolean, Returns false if the test fails and true if the test passes.
+	 */
 	public boolean checkBoxes() {
 		boolean[] cellCompareArray = new boolean[magicNum + 1];
 		int compareNum = 0;
@@ -207,19 +231,27 @@ public class SudokuBruteForceSolver
 					}
 				}
 			}
-
 			if (x + width < magicNum) {
 				x = x + width;
 			} else {
 				x = 0;
 				y = y + height;
 			}
-
 			i++;
 		}
 		return true;
 	}
 
+	public void setMagicNum(int magicNum) {
+		this.magicNum = magicNum;
+	}
+
+	/**
+	 * The printInformation method prints out the current puzzle, whether it was solved or not, and the time in milliseconds the puzzle takes to solve.
+	 * @param solved - A boolean value which determines if the puzzle was solved
+	 * 				 - If false, "Current Sudoku can not be solved." is printed along with the unsolved Sudoku and the taken time in milliseconds.
+	 * 				 - If true, "Current Sudoku was solved!" is printed along with the solved Sudoku with its solution and the time taken in milliseconds.	
+	 */
 	public void printInformation(boolean solved) {
 		int[][] finalArray;
 		if (solved == false) {
@@ -229,7 +261,7 @@ public class SudokuBruteForceSolver
 			System.out.println("Current Sudoku was solved!");
 			finalArray = testArray;
 		}
-
+		
 		int i = 0;
 		for (int rowCount = 0; rowCount < magicNum; rowCount++) {
 			if ((i % height) == 0) 
@@ -251,6 +283,10 @@ public class SudokuBruteForceSolver
 		printOutline();
 	}
 
+	/**
+	 * The printOutline method sets up the outlines of the printed Sudoku in the printInformation method.
+	 * 
+	 */
 	public void printOutline() {
 		int i = 0;
 		int j = 0;
